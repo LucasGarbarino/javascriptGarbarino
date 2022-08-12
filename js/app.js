37e247201706b65da315d000b9258dfa71446f46
Lucas Garbarino <lucasgarbarino97@gmail.com>
2022-08-11 22:12:07 -0300

    primera entrega

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 let cant = 0
+const tareas = []
+
+class Tarea {
+    constructor (titulo, descripcion, cant, fecha) {
+        this.titulo = titulo
+        this.descripcion = descripcion
+        this.numero = cant
+        this.fecha = fecha
+    }
+}
 
 function nuevaTarea() {
     // debugger
     let continuar = true
     while(continuar) {
-        let tarea = prompt('¿Qué vamos a hacer hoy?')
-        if (tarea == '') {
+        let titulo = prompt('¿Qué hay que hacer?')
+        let descripcion = prompt('Añade una descripción')
+        let fecha = new Date()
+        if (titulo == '') {
             alert('No te hagas el loco')
-        } else if(tarea != null) {
+        } else if(titulo != null) {
             cant = cant + 1
-            console.log(tarea)
+            console.log(titulo, descripcion)
             if(cant==2) {
                 console.warn('Ahí va queriendo')
             } if(cant == 4) {
@@ -18,11 +30,17 @@ function nuevaTarea() {
                 console.warn('Epa! Tomatelo con soda, no?')
             }
         }
+        tareas.push(new Tarea(titulo, descripcion, cant, fecha))
         continuar = confirm('¿Agregar nueva tarea?')
     } 
     if(continuar == false && cant <= 2) {
         console.warn('Ponele un poco de onda, che')
     }
 }
-    
-    
+
+function queTareas() {
+    tareas.forEach(tarea => {
+        console.table(tarea)
+    })
+}
+
